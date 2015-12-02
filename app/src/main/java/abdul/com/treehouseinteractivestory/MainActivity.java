5a1315bb275f0f9ab52c3c41d650169912962033
Abdul Rahimi <afr114@gmail.com>
2015-12-02 12:00:14 -0800

    create activity for home screen

--- app/src/main/java/abdul/com/treehouseinteractivestory/MainActivity.java
+++ app/src/main/java/abdul/com/treehouseinteractivestory/MainActivity.java
@@ -1,7 +1,12 @@
 package abdul.com.treehouseinteractivestory;
 
+import android.app.SearchManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText mNameField = (EditText) findViewById(R.id.nameEditText);
+        Button mStartButton = (Button) findViewById(R.id.startButton);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mNameField.getText().toString();
+                Toast.makeText(MainActivity.this,name, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
